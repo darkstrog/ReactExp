c8f300f79b134b524f54fbad82c4ab12404f288a
darkstrog <demonstrog@yandex.ru>
2020-10-26 16:38:29 +0300

    commit

---
+++ rexp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
+
+function App() {
+  return (
+    <div className="App">
+          <header className="App-header">
+              <Button>Тык</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
